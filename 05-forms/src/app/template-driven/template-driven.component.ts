0911c312f75113559684f80bc1a6b3f40520f7fa
PopovDmytro <dimahom@ukr.net>
2018-01-16 22:44:57 +0200

    done lessons 86 - 101

---
+++ 05-forms/src/app/template-driven/template-driven.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-template-driven',
+  templateUrl: './template-driven.component.html',
+  styleUrls: ['./template-driven.component.css']
+})
+export class TemplateDrivenComponent implements OnInit {
+  user = {
+    username: 'One',
+    email: 'email@com.com',
+    pass: 'test'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(this.user);
+  }
+
+}
--- recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,23 +1,50 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 import { Recipe } from "../recipe";
 import {ShoppingListService} from "../../shopping-list";
+import {Subscription} from "rxjs/Rx";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent implements OnInit {
-  @Input() selectedRecipe: Recipe;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  selectedRecipe: Recipe;
+  private recipeIndex: number;
+  private subscribtion: Subscription;
 
-  constructor(private sls: ShoppingListService) { }
+  constructor(private sls: ShoppingListService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private recipesService: RecipeService) { }
 
   ngOnInit() {
+    this.subscribtion = this.route.params.subscribe(
+      (params: any) => {
+        this.recipeIndex = params['id'];
+        this.selectedRecipe = this.recipesService.getRecipe(this.recipeIndex);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscribtion.unsubscribe();
   }
 
   onAddToShoppingList() {
     this.sls.addItems(this.selectedRecipe.ingredients);
+  }
+
+  onEdit() {
+    this.router.navigate(['/recipes', this.recipeIndex, 'edit']);
+  }
+
+  onDelete() {
+    this.recipesService.deleteRecipe(this.selectedRecipe);
+    this.router.navigate(['/recipes']);
   }
 
 }
