678072808422f53cd52117e12febe85a7b23438b
PopovDmytro <dimahom@ukr.net>
2018-03-02 23:12:11 +0200

    last lesson 15

--- study/src/app/app.module.ts
+++ study/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses/courses.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CoursesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
