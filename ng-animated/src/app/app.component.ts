b8c1355cd414ba52c75dba117d24ff2f8ac7c000
PopovDmytro <dimahom@ukr.net>
2018-05-13 22:03:49 +0300

    goal component

--- ng-animated/src/app/app.component.ts
+++ ng-animated/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  courseGoals = [
+    {title: "Master Ng Styling", isActiveGoal: true},
+    {title: "Understand Ng Animations", isActiveGoal: false},
+    {title: "Master Ng Animations", isActiveGoal: false},
+  ]
 }
