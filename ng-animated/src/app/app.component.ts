60d6b291f05d6378814228099fec765d78ca5d34
PopovDmytro <dimahom@ukr.net>
2018-05-14 23:15:40 +0300

    ng styles

--- ng-animated/src/app/app.component.ts
+++ ng-animated/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +10,15 @@ export class AppComponent {
     {title: "Master Ng Styling", isActiveGoal: true},
     {title: "Understand Ng Animations", isActiveGoal: false},
     {title: "Master Ng Animations", isActiveGoal: false},
-  ]
+  ];
+  isFavorite = false;
+
+  constructor(private renderer: Renderer2) {
+
+  }
+
+  onShowBoring(element: HTMLElement) {
+    // element.style.display = 'block';
+    this.renderer.setStyle(element, 'display', 'block');
+  }
 }
