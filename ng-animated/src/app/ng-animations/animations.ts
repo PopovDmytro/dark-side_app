a04efe7997fc8aa9c5492aed7fb9a0c0828f814f
PopovDmytro <dimahom@ukr.net>
2018-05-19 17:17:14 +0300

    states wildcard '*' :enter :leave

--- ng-animated/src/app/ng-animations/animations.ts
+++ ng-animated/src/app/ng-animations/animations.ts
@@ -1,20 +1,25 @@
 import {animate, state, style, transition, trigger} from "@angular/animations";
 
 export const showStateTrigger = trigger('showState', [
-  state('notShown', style({
-
-  })),
-  state('shown', style({
-
-  })),
-  transition('void => shown',[
+  // transition('void => *',[
+  //   style({
+  //     left: '100%',
+  //     opacity: 0
+  //   }),
+  //   animate(300)
+  // ]),
+  // transition('shown => void',animate(300, style({
+  //   left: '100%',
+  //   opacity: 0
+  // })))
+  transition(':enter',[
     style({
       left: '100%',
       opacity: 0
     }),
     animate(300)
   ]),
-  transition('shown => void',animate(300, style({
+  transition(':leave',animate(300, style({
     left: '100%',
     opacity: 0
   })))
